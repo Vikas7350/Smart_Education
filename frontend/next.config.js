91e65194391a5eb7c96927f0c165eada61613f55
Vikas7350 <choudharivikas331@gmail.com>
2025-11-09 11:44:17 +0530

    Fix alias paths for Vercel2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,22 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost'],
+  },
+  webpack: (config) => {
+    const srcPath = path.join(__dirname, 'src')
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': srcPath,
+    }
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, 'src'),
+    ]
+    return config
   },
 }
 
